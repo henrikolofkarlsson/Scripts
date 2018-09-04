9b16b8bf9f748994ba1b6354f82c58caf9069d49
Henrik Karlsson <p.h.o.karlsson@gmail.com>
2018-09-04 12:20:18 +0200

    First

---
+++ Laser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        if (transform.position.y > 8)
+        {
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            Destroy(this.gameObject);
+        }
+	}
+}
